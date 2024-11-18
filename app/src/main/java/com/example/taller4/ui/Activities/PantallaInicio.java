fda1d285080bb14c80e1ddd9fb0d9efd4b0f5de4
Samuu10 <svallmun@myuax.com>
2024-11-18 20:27:00 +0100

    Commit actualización

--- app/src/main/java/com/example/taller4/ui/Activities/PantallaInicio.java
+++ app/src/main/java/com/example/taller4/ui/Activities/PantallaInicio.java
@@ -1,4 +1,20 @@
 package com.example.taller4.ui.Activities;
 
-public class PantallaInicio {
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.taller4.R;
+
+public class PantallaInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_inicio);
+
+        findViewById(R.id.boton_pantalla_principal).setOnClickListener(v -> {
+            startActivity(new Intent(PantallaInicio.this, PantallaPrincipal.class));
+        });
+    }
+
 }
--- app/src/main/java/com/example/taller4/ui/Activities/PantallaPrincipal.java
+++ app/src/main/java/com/example/taller4/ui/Activities/PantallaPrincipal.java
@@ -1,4 +1,28 @@
 package com.example.taller4.ui.Activities;
 
-public class PantallaPrincipal {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.taller4.R;
+
+public class PantallaPrincipal extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_principal);
+
+
+        findViewById(R.id.boton_añadir).setOnClickListener(v -> mostrarDialogoAñadirProducto());
+        findViewById(R.id.boton_eliminar).setOnClickListener(v -> mostrarDialogoEliminarProducto());
+    }
+
+    public void mostrarDialogoAñadirProducto() {
+
+    }
+
+    public void mostrarDialogoEliminarProducto() {
+
+    }
 }
